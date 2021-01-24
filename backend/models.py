a73733693a505782500347bab38b87ae683041e8
Aamsi <ismaelouali45@gmail.com>
2021-01-25 00:40:50 +0100

    first commit

---
+++ backend/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from pydentic import BaseModel
+
+Base = declarative_base()
+
+class User(BaseModel):
+    first_name: str
+    last_name: str = None
+    age: int
+
+    class Config:
+        orm_mode = True
