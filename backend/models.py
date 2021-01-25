331cfd8e2471cb8a2a8ef775eea34c9d11a0f482
Aamsi <ismaelouali45@gmail.com>
2021-01-25 10:14:32 +0100

    pydentic --> pydantic

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-from pydentic import BaseModel
+from pydantic import BaseModel
 
 Base = declarative_base()
 
