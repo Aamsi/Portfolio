d98d2b4947c98b1d1d068280de5b5ac67d920612
Aamsi <ismaelouali45@gmail.com>
2021-02-04 22:08:59 +0100

    WIP: adds project component

--- frontend/src/components/Project.vue
+++ frontend/src/components/Project.vue
@@ -1,9 +1,89 @@
 <template>
-    <h1>A REMPLIR</h1>
+    <v-card
+    class="mx-auto"
+    max-width="344"
+    dark
+  >
+    <v-img
+      :src="image.source"
+      :alt="image.alt"
+      height="200px"
+      white
+    ></v-img>
+
+    <v-card-title>
+      {{ title }}
+    </v-card-title>
+
+    <v-card-subtitle>
+      {{ generateShortDescription }}
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="dark lighten-2"
+        text
+        :href="url"
+        target='_blank'
+      >
+        Repository
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{ description }}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
 </template>
 
 <script>
 export default {
-    name: 'Project'
+    name: 'Project',
+    props: {
+        image: {
+            type: Object,
+            required: false,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: URL,
+            required: true
+        },
+    },
+    data() {
+        return {
+            short_description: "En cours..."
+        }
+    },
+    computed: {
+        generateShortDescription() {
+            let short_description = this.description.substring(0, 15) + '...';
+            if (!short_description)
+                return "En cours...";
+            return short_description;
+        },
+    }
 }
 </script>
--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -1,5 +1,13 @@
 <template>
-    <Project/>
+    <Project
+        :image="{
+            source: 'https://i.picsum.photos/id/76/200/200.jpg?hmac=psXm-BMm4NaZlO_dAn3FH6ahWyqXeDxPEkz5hU8XVqM',
+            alt: 'Photo non fournie'
+        }"
+        :title="'Test'"
+        :description="'Test description'"
+        :url="'https://github.com/Aamsi/Portfolio'"
+    />
 </template>
 
 <script>
