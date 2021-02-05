e59bfaeb11f5844aabfea35d11a942d3036b8924
Aamsi <ismaelouali45@gmail.com>
2021-02-05 19:44:18 +0100

    add footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,11 +7,36 @@
         <v-tab to="/about">À propos de moi</v-tab>
     </v-tabs>
     </v-app-bar>
-  </div>
-  <router-view />
+    <router-view />
+    </div>
 
-
-
+    <v-footer
+    dark
+    padless
+    fixed
+    >
+      <v-card
+      class="flex text-center"
+      flat
+      tile
+      >
+        <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        dark
+        icon
+        centered
+        :href="icon.url"
+        target="_blank"
+        >
+        <v-icon size="24px"> {{ icon.name }} </v-icon>
+        </v-btn>
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} — <strong>OUALI Ismael</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
 
   </v-app>
 </template>
@@ -25,7 +50,16 @@ export default {
   },
 
   data: () => ({
-    //
+    icons: [
+      {
+        name: 'mdi-linkedin',
+        url: 'https://www.linkedin.com/in/isma%C3%ABl-ouali-86b3a3192/'
+      },
+      {
+        name: 'mdi-github',
+        url: 'https://github.com/Aamsi'
+      }
+    ]
   }),
 };
 </script>
