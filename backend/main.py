abcc71d15c1b414525c48db4cab583c158a0f245
Aamsi <ismaelouali45@gmail.com>
2021-02-06 13:17:57 +0100

    adds allowed_origins to backend app

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware, db
+from fastapi.middleware.cors import CORSMiddleware
+
 from models import Category, Project
 from schema import PydanticProject, PydanticCategory
 from dotenv import load_dotenv
@@ -12,6 +14,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:8080"])
 app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 app.include_router(projects.router)
