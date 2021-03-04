4bb9a0ece7603c58624a1a4906c4e9c25f2c2a49
Aamsi <ismaelouali45@gmail.com>
2021-03-04 13:21:02 +0100

    insert categories before projects

--- backend/insert_fixtures.py
+++ backend/insert_fixtures.py
@@ -7,10 +7,15 @@ class InsertFixtures():
     def __init__(self, file_name):
         self.file_name = file_name
         self.projects = self.get_projects()
+        self.categories = self.get_categories()
 
     def get_projects(self):
         with open(self.file_name, 'r') as f:
             return json.load(f)['projects']
+
+    def get_categories(self):
+        with open(self.file_name, 'r') as f:
+            return json.load(f)['categories']
 
     def post_projects(self):
         for project in self.projects:
@@ -18,6 +23,13 @@ class InsertFixtures():
             response = requests.post("http://localhost:3080/projects/create/", json=project)
             response.raise_for_status()
 
+    def post_categories(self):
+        for category in self.categories:
+            print(category)
+            response = requests.post("http://localhost:3080/categories/create/", json=category)
+            response.raise_for_status()
+
 
 obj = InsertFixtures('fixtures.json')
+obj.post_categories()
 obj.post_projects()
